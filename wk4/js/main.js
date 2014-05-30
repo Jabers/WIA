7e38941ced0a7adc0eb58fa0c8da18d462e80a6b
JB SANCHEZ <jabers@Js-iMac.local>
2014-05-30 12:20:05 -0600

    jQuery fade in/out effects applied
    to club vs imdi running graphics

--- wk4/js/main.js
+++ wk4/js/main.js
@@ -4,6 +4,30 @@ $(function() {
 		
 			$("#vidCont").slideToggle(500);
 		
-		});		
+		});
+
+	$("#fadeinclub").click(function(){
+		
+			$("#vs_club").fadeIn(1000);
+		
+		});
+
+	$("#fadeoutclub").click(function(){
+			$("#vs_club").fadeOut(1000);
+		
+		
+		});
+
+	$("#fadeinindi").click(function(){
+		
+			$("#vs_indi").fadeIn(1000);
+		
+		});
+
+	$("#fadeoutindi").click(function(){
+			$("#vs_indi").fadeOut(1000);
+		
+		
+		});
 
 });
