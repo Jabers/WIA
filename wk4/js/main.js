8b50ee00b577a14877f9a2360c489fb308705831
JB SANCHEZ <jabers@Js-iMac.local>
2014-05-30 13:00:49 -0600

    canvas used twice
    switched how to display club vs imdi benefits by using canvas instead
    of a bg image for a div

--- wk4/js/main.js
+++ wk4/js/main.js
@@ -27,7 +27,34 @@ $(function() {
 	$("#fadeoutindi").click(function(){
 			$("#vs_indi").fadeOut(1000);
 		
-		
 		});
+
+	window.onload = function() {
+		var canvas1 = document.getElementById('Canvas1');
+		if (canvas1 && canvas1.getContext) {
+			var ctx = canvas1.getContext("2d");
+				if (ctx) {
+
+					var  srcImg = document.getElementById("img1");
+						
+					//Draw an image directly onto the canvas
+					ctx.drawImage(srcImg, 0,0);			
+			     						
+					}
+		}
+
+		var canvas2 = document.getElementById('Canvas2');
+		if (canvas2 && canvas2.getContext) {
+			var ctx = canvas2.getContext("2d");
+				if (ctx) {
+
+					var  srcImg = document.getElementById("img2");
+						
+					//Draw an image directly onto the canvas
+					ctx.drawImage(srcImg, 0,0);			
+			     						
+					}
+		}
+	}
 
 });
